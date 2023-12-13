b3e243936408bc333a33b7221151b7bc0945d55a
Rajendra pathak <rajendrapathak1988@gmail.com>
2023-12-13 15:52:36 +0530

    added nodejs cluster for better perfomence

--- index.js
+++ index.js
@@ -1,63 +1,65 @@
-// app.js or index.js
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const postRoutes = require('./routes/post.js');
-const userRoutes = require('./routes/user.js');
-const helmet = require('helmet');
-const cors = require('cors'); // Import the cors middleware
-const morgan = require('morgan');
-const { globalLimiter, specificLimiter } = require('./utils/rateLimiter.js')
-const Post = require('./models/post');
-require('dotenv').config(); // Load environment variables from .env file
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.set('view engine', 'ejs');
-
-const connectToDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log("DB connected");
-  } catch (error) {
-    console.error("Error connecting to DB:", error);
-    process.exit(1);
+if (cluster.isMaster) {
+  // Fork workers
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
-};
 
-connectToDB();
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
+} else {
+  // Worker code
+  const express = require('express');
+  const mongoose = require('mongoose');
+  const bodyParser = require('body-parser');
+  const postRoutes = require('./routes/post.js');
+  const userRoutes = require('./routes/user.js');
+  const helmet = require('helmet');
+  const cors = require('cors');
+  const morgan = require('morgan');
+  const { globalLimiter, specificLimiter } = require('./utils/rateLimiter.js');
+  const Post = require('./models/post');
+  require('dotenv').config();
 
+  const app = express();
+  const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(bodyParser.json());
-app.use(helmet())
-app.use(globalLimiter)
+  app.set('view engine', 'ejs');
 
-// Enable CORS for all routes
-app.use(cors());
+  const connectToDB = async () => {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+      console.log("DB connected");
+    } catch (error) {
+      console.error("Error connecting to DB:", error);
+      process.exit(1);
+    }
+  };
 
-// Use the "combined" pre-defined format
-app.use(morgan('combined'));
+  connectToDB();
 
-app.get('/blogs', async (req, res) => {
-  try {
-    const blogs = await Post.find({}, { title: 1, content: 1, author: 1, createdAt: 1 });
-    res.render('blogs', { blogs }); // Render the 'blogs' view and pass the blogs data
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+  app.use(bodyParser.json());
+  app.use(helmet());
+  app.use(globalLimiter);
+  app.use(cors());
+  app.use(morgan('combined'));
 
-// Routes
-app.use('/api', postRoutes);
-app.use('/api', userRoutes);
+  app.get('/blogs', async (req, res) => {
+    try {
+      const blogs = await Post.find({}, { title: 1, content: 1, author: 1, createdAt: 1 });
+      res.render('blogs', { blogs });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  app.use('/api', postRoutes);
+  app.use('/api', userRoutes);
 
-
-
-module.exports = { app, mongoose };
+  app.listen(PORT, () => {
+    console.log(`Worker ${process.pid} is running on port ${PORT}`);
+  });
+}
